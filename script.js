2bcbbfe93f328de734435d44c3aaeadb5fe721df
Kaelyn Syrdahl <kasyrdahl@ucdavis.edu>
2024-01-04 12:35:13 -0800

    edit name link

--- script.js
+++ script.js
@@ -4,13 +4,18 @@
     console.log('reading js');
 
     const overlay = document.querySelector('#overlay');
-    const header = document.querySelector('header');
+    // const indeximg = document.querySelector('#index img');
 
-    header.addEventListener('click', function (event){
-        console.log('clicked me');
-        location.reload();
-    });
+    // document.querySelector('.more').addEventListener('mouseover', function (event){
+    //     console.log("i was touched");
+    //     indeximg.style.transform = 'scale(1.1)';
+    // });
 
+    // document.querySelector('.more').addEventListener('mouseout', function (event){
+    //     console.log("i was touched");
+    //     indeximg.style.transform = 'scale(1)';
+    // });
+    
 // Overlay for navigation drop down
 
     document.querySelector('#desktop').addEventListener('click', function (event){
@@ -26,13 +31,5 @@
             overlay.className = 'showing';
         }
     });
-
-// Reloads page when you click on header
-
-    header.addEventListener('click', function (event){
-        console.log('clicked me');
-        location.reload();
-    });
-
 
 })();
